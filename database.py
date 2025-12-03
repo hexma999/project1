68fc32d0563f5ff8c6c33dec9dc8bdb664e19b1d
hexma999 <hexma999@naver.com>
2025-12-03 22:36:37 +0900

    db 설정

--- database.py
+++ database.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # 데이터베이스 URL 정의
-DATABASE_URL = "mysql+pymysql://apple:1111@localhost:3307/aidetect"
+#DATABASE_URL = "mysql+pymysql://apple:1111@localhost:3307/aidetect"
+DATABASE_URL = "mysql+pymysql://admin:dnjsgmlwhgdk@team-db.cf4wuo0gsigf.ap-southeast-2.rds.amazonaws.com:3306/aidetect"
 
 # 엔진 생성
 engine = create_engine(DATABASE_URL, echo=True)
