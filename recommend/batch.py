980c775a08e67b5d9e4348792aed49763afd95ab
권경수 <hexma999@naver.com>
2025-12-10 17:46:08 +0900

    KAN-34 추천기능 테스트 기능 추가

---
+++ recommend/batch.py
@@ -0,0 +1,16 @@
+import time
+import schedule
+import subprocess
+
+def run_recommendation():
+    print("추천 로직 실행 중...")
+    subprocess.run(["python", "recommend.py"])
+
+# 매 1시간마다 실행
+#schedule.every(1).hours.do(run_recommendation)
+#schedule.every(1).minutes.do(run_recommendation)
+schedule.every(5).seconds.do(run_recommendation)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
