424a3eb81ab201827edf046044b534ce9a7fa4e0
권경수 <hexma999@naver.com>
2025-12-15 16:34:35 +0900

    배치 주기 변경

--- recommend/batch.py
+++ recommend/batch.py
@@ -7,9 +7,9 @@ def run_recommendation():
     subprocess.run(["python", "recommend.py"])
 
 # 매 1시간마다 실행
-#schedule.every(1).hours.do(run_recommendation)
+schedule.every(1).hours.do(run_recommendation)
 #schedule.every(1).minutes.do(run_recommendation)
-schedule.every(5).seconds.do(run_recommendation)
+#schedule.every(5).seconds.do(run_recommendation)
 
 while True:
     schedule.run_pending()
